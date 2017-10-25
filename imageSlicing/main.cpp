e6b7669925c77260825fd1d553a7e478abe0a2bd
edusjor <solanojorge77@gmail.com>
2017-10-25 04:38:44 -0600

    Actualizacion de Comentarios

--- imageSlicing/main.cpp
+++ imageSlicing/main.cpp
@@ -5,6 +5,12 @@
 using namespace cv;
 using namespace std;
 
+//Para compilar:    g++ -Wall -o run  main.cpp `pkg-config --cflags --libs opencv`
+//Para correrlo:    ./run
+
+
+
+
 
 int main (){
     Mat imagen=imread("img1.png",1);            //carga la imagen
@@ -16,28 +22,30 @@ int main (){
 
 
     double area_imagen=img_Width*img_Height;
-    double area_trozo=area_imagen/cantidad;
+    double area_trozo=area_imagen/cantidad;    //calcula el area que deberia medir cada trozo
 
-    double trozo_Lado=pow(area_trozo,0.5);
+    double trozo_Lado=pow(area_trozo,0.5);      //saca la raiz cuadrada para calcular el lado de cada trozo
 
     Mat array_De_Trozos[cantidad];              //crea un array para la cantidad de trozos que va a guardar
 
 
-    int posi_X=0;                            //posicion en la que se va a cortar los trozos
-    int posi_Y=0;
 
-    int cant_x=img_Width/trozo_Lado;
-    int cant_y=img_Height/trozo_Lado;
+
+    int posi_X=0;                               //posicion x en la que se va a cortar los trozos
+    int posi_Y=0;                               //posicion y en la que se va a cortar los trozos
+
+    int cant_x=img_Width/trozo_Lado;            //cantidad de filas que debe tener la nueva imangen
+    int cant_y=img_Height/trozo_Lado;           //cantidad de columnas
 
     int contador=0;
     for (int y=0; y<cant_y; y++) {
         for (int x = 0; x < cant_x; x++) {
-            Rect myROI(posi_X, posi_Y, trozo_Lado, trozo_Lado);        //posix posiy width   height
+            Rect myROI(posi_X, posi_Y, trozo_Lado, trozo_Lado);        //posix posiy width   height          crea el rectangulo/cuadrado con las medidas y la posicion para copiar de la u
 
             posi_X += trozo_Lado;                                     //aumenta en la cantidad cortada la posicion en x
 
             Mat trozo_imagen = imagen(myROI);                       //corta la imagen segun el tamano y posicion del rectangulo creado
-            array_De_Trozos[contador] = trozo_imagen;
+            array_De_Trozos[contador] = trozo_imagen;               //guarda cada nuevo trozo en un array con todos los trozos
             contador++;
         }
         posi_X=0;
@@ -45,8 +53,44 @@ int main (){
     }
 
 
-    Mat array_filas[cant_y];
 
+
+
+
+    //desordena el array
+    Mat desordenado[cantidad];
+    bool usado[cantidad];
+
+
+    for(int i=0; i<cantidad;i++){
+        usado[i]= false;
+    }
+    int index=0;
+    for (int i=0; i<cantidad;i++){
+        srand (time(NULL));
+        do{
+
+            index =(rand() % cantidad);
+        }while (usado[index]);
+        desordenado[i]=array_De_Trozos[index];
+        usado[index]=true;
+    }
+    for (int i =0;i<cantidad;i++){
+        array_De_Trozos[i]=desordenado[i];
+    }
+
+
+
+
+
+
+
+
+
+    Mat array_filas[cant_y];        //array con todas las filas
+
+
+    //concatena los trozos de cada fila y guarda cada fila en un array
     int cont1 = 0;
     for (int y=0; y<cant_y; y++) {
         Mat Fila=array_De_Trozos[cont1];
@@ -60,6 +104,7 @@ int main (){
         array_filas[y]=Fila;
     }
 
+    //toma las filas guardadas en ael array y las acomoda una encima de otra para que quede la nueva imagen con el tamano de la principal original
     Mat Columnas=array_filas[0];
     for (int g=1; g<cant_y; g++){
         vconcat(Columnas, array_filas[g], Columnas);
